57fcc53ad6c98c3c3695ed183f1c75b2852e4e72
Damian Moore <damian.moore@tandem.co.uk>
2018-10-11 11:49:00 +0100

    Admin setup

--- dataset/admin.py
+++ dataset/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Dataset, Datapoint, Label, UserLabel
+
+
+class DatasetAdmin(admin.ModelAdmin):
+    pass
+
+class DatapointAdmin(admin.ModelAdmin):
+    pass
+
+class LabelAdmin(admin.ModelAdmin):
+    pass
+
+class UserLabelAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Dataset, DatasetAdmin)
+admin.site.register(Datapoint, DatapointAdmin)
+admin.site.register(Label, LabelAdmin)
+admin.site.register(UserLabel, UserLabelAdmin)
